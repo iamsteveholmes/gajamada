d5507cd5ccc57e2f6d4bb1cf59bbcb78051de596
Steve Holmes <iamsteveholmes@gmail.com>
2012-11-11 15:22:31 -0800

    adding bootstrapping

--- grails-app/domain/com/gajamada/Job.groovy
+++ grails-app/domain/com/gajamada/Job.groovy
@@ -2,5 +2,6 @@ package com.gajamada
 
 class Job {
 
+    String name
     static hasMany = [runs: JobRun]
 }
---
+++ grails-app/jobs/com/gajamada/MonitorJob.groovy
@@ -0,0 +1,14 @@
+package com.gajamada
+
+class MonitorJob {
+
+    MonitorService monitorService
+
+    static triggers = {
+      simple repeatInterval: 5000l // execute job once in 5 seconds
+    }
+
+    def execute() {
+        // execute job
+    }
+}
